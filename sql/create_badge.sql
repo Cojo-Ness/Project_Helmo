7213837d562b0213dacb91fc01cbbbfe5fc758a7
Fixer <fixer3838@live.fr>
2020-08-07 02:39:44 +0200

    MODIFIED image field, can be NULL for dev

--- sql/create_badge.sql
+++ sql/create_badge.sql
@@ -2,5 +2,5 @@ CREATE TABLE col_badge (
 	id_badge INT PRIMARY KEY AUTO_INCREMENT,
     nom varchar(100) NOT NULL,
     description text NOT NULL,
-    image varchar(255) NOT NULL
+    image varchar(255)
 );
